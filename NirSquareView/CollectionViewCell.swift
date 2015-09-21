d094ab25fa82dbfaf4ec4fc4c995832d4fd41a47
NSNirvana <NSNirvana@iCloud.com>
2015-09-21 16:31:23 +0800

    基本已完成，准备进行进一步抽象和适配

---
+++ NirSquareView/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  NirSquareView
+//
+//  Created by Nirvana on 9/21/15.
+//  Copyright © 2015 NSNirvana. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
--- NirSquareView/ViewController.swift
+++ NirSquareView/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.imageView.layer.cornerRadius = 22
+        cell.imageView.clipsToBounds = true
+        print(cell.imageView.frame)
+        return cell
     }
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let totalHeight = self.collectionView.frame.height
+        return CGSizeMake(totalHeight*0.4396, totalHeight*0.5)
+    }
 
 }
 
